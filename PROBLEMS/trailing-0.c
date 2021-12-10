3e4ca8b9cd7c8bcf26050c65546312ed2920563d
MdSaifulIslam <saiful12@cse.pstu.ac.bd>
2021-12-10 15:05:35 +0600

    trailing zero - 0

---
+++ PROBLEMS/trailing-0.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+
+    int n, i, count = 0;
+    scanf("%d", &n);
+    
+    for(i = 5; n/i>1; i *= 5){
+        count += n/i;
+    }
+
+    printf("Total zeroes-0 is trailing: %d\n", count);
+    return 0;
+}
